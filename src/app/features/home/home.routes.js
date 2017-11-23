9a28794c35a1f1706bc84c55c0518e35b2a98cc5
Charles Olupot <olupotcharles@gmail.com>
2017-11-23 13:03:19 +0300

    Seperating Upload from Download

--- src/app/features/home/home.routes.js
+++ src/app/features/home/home.routes.js
@@ -5,15 +5,6 @@ export default function routes($stateProvider) {
         .state('home', {
             url: '/',
             template: require('./home.html'),
-            resolve: {
-                dataSets: ['Data', '$http', function (Data, $http) {
-                    return Data.getMany('dataSets', {
-                        paging: false,
-                        translate: true,
-                        fields: 'id,name,uuid,displayName,periodType,dataEntryForm[htmlCode],organisationUnits[id,name,displayName]'
-                    });
-                }]
-            },
             controller: 'HomeController',
             controllerAs: 'home'
         });
--- src/app/features/home/index.js
+++ src/app/features/home/index.js
@@ -8,11 +8,10 @@ import routing from './home.routes';
 import ModalController  from './modal.controller'
 import HomeController from './home.controller';
 import Data from '../../services/data.service';
-import Filters from '../../services/filters.service';
 import Utils from '../../services/utils.service'
 
 
-export default angular.module('app.home', [uirouter, Data, Filters, Utils])
+export default angular.module('app.home', [uirouter, Data, Utils])
     .config(routing)
     .controller('ModalController', ModalController)
     .controller('HomeController', HomeController)
